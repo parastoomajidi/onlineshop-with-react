1c4f8c69f99c408043ffb50abdf7cc8b42ba019f
Hsis8hJ39@&@£×92992ajue92024 <parasto.majidi0@gmail.com>
2025-08-02 10:47:42 +0330

    Daily Practise-Conditional render

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
- return(
-  <div>
-    <Job salary={20000} posiotion="Product designerin" company="google"/>
-    <Job salary={8000} posiotion="Developer" company="Audi"/>
-  </div>
- )
-}
-// component 
-const Job =(props)=>{
+  const age = 25;
   return(
-   <div>
-     <h2>{props.salary}</h2>
-    <h2>{props.posiotion}</h2>
-    <h2>{props.company}</h2>
+   <div className='App'>
+    {age >34 ? <h1>faild</h1> : <h1>passed</h1>}
+    <button type='submit' className={age>24 ? "s" : "r" }>click me</button>
    </div>
   )
 }
 
 
+
 export default App;
+
+
+
+//   <div>
+//     <Job salary={20000} posiotion="Product designerin" company="google"/>
+    
+//   </div>
+//  )
+// }
+
+// const Job =(props)=>{
+//   return(
+//    <div>
+//      <h2>{props.salary}</h2>
+//     <h2>{props.posiotion}</h2>
+//     <h2>{props.company}</h2>
+//    </div>
+//   )
