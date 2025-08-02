b7ced3d7c25ec0d055f44825b9f64051b4bb00a4
Hsis8hJ39@&@£×92992ajue92024 <parasto.majidi0@gmail.com>
2025-08-02 09:43:57 +0330

    Daily Practise-Task1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return(
+  <div>
+    <Job salary={20000} posiotion="Product designerin" company="google"/>
+    <Job salary={8000} posiotion="Developer" company="Audi"/>
+  </div>
+ )
 }
+// component 
+const Job =(props)=>{
+  return(
+   <div>
+     <h2>{props.salary}</h2>
+    <h2>{props.posiotion}</h2>
+    <h2>{props.company}</h2>
+   </div>
+  )
+}
+
 
 export default App;
