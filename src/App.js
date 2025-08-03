eb2e001f6294eb237ef09d76bfe09054c440c2e3
Hsis8hJ39@&@£×92992ajue92024 <parasto.majidi0@gmail.com>
2025-08-03 08:23:34 +0330

    Daily Practise-UseState-show-hide button

--- src/App.js
+++ src/App.js
@@ -4,19 +4,41 @@ import {useState} from 'react';
 
 // task 2 map the list
 function App() {
-  const [age, setAge] = useState(0);
+  // const [age, setAge] = useState(0);
+  // const increaseAge = ()=>{
+  //   setAge(age +1)
+  // }
+  // const decreaseAge =()=>{
+  //   setAge(age -1)
+  // }
+  // return<div className='App'>
+  //   <h1>{age}</h1>
+  //   <button onClick={increaseAge}>Increase age</button>
+  //   <button onClick={decreaseAge}> Decearese age</button>
+  // </div>
+  /////////////////////////////////////////////////////
+  // const [inputvalue, setInputValue]= useState("")
+  // const handelInputChange =()=>{
+  //   console.log(event.target.value)
+  //   setInputValue(event.target.value)
+  // }
+  // return<div className='App'>
+  //   <input type='text' onChange={handelInputChange}></input>
+  //   <h1>{inputvalue}</h1>
+  // </div>
 
-  const increaseAge = ()=>{
-    setAge(age +1)
-  }
-  const decreaseAge =()=>{
-    setAge(age -1)
-  }
+  const [showText, setShowText]= useState(true);
+
+  const toggelText =()=>{
+    // update stat
+    setShowText(!showText);
+
 
   return<div className='App'>
-    <h1>{age}</h1>
-    <button onClick={increaseAge}>Increase age</button>
-    <button onClick={decreaseAge}> Decearese age</button>
+    <button onClick={toggelText} > show/heide</button>
+    {showText && <h1>Parastoo Style codeing</h1>}
+
+
   </div>
 
 }
