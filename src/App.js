e43f6b8643029e58746bcce9200e8105d5734026
Hsis8hJ39@&@£×92992ajue92024 <parasto.majidi0@gmail.com>
2025-08-03 07:54:33 +0330

    Daily Practise-UseState

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 // task 2 map the list
 function App() {
-  const classes =[
-    {name:"html",finished: true},
-    {name:"JS", finished: false},
-    {name:"CSS", finished: true}
-  ];
+  const [age, setAge] = useState(0);
 
-  return(
-    <div className='App'>
-      {classes.map((c, index)=>{
-        // return< Class1 key={index} name={classes.name} finished={classes.finished}/>
-        return c.finished && <h2 key={index}>{c.name}</h2>
-      })}
-    </div>
-  )
-}
-const Class1 =(props)=>{
-  return( <h2>{props.name} : {props.finished}</h2>)
+
+
+  
+  const increaseAge = ()=>{
+    setAge(age +1)
+  }
+
+  return<div className='App'>
+    <h1>{age}</h1>
+    <button onClick={increaseAge}>Increase age</button>
+  </div>
+
 }
 
 export default App;
 
 
 
-//   <div>
-//     <Job salary={20000} posiotion="Product designerin" company="google"/>  
-//   </div>
-//  )
-// }
-
-// const Job =(props)=>{
-//   return(
-//    <div>
-//      <h2>{props.salary}</h2>
-//     <h2>{props.posiotion}</h2>
-//     <h2>{props.company}</h2>
-//    </div>
-//   )
