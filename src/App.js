095e55bf18bc8d0ee3d443feb059e724b9ffa9bc
Hsis8hJ39@&@£×92992ajue92024 <parasto.majidi0@gmail.com>
2025-08-03 07:58:11 +0330

    Daily Practise-UseState-increse and decrease button

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import {useState} from 'react';
 function App() {
   const [age, setAge] = useState(0);
 
-
-
-  
   const increaseAge = ()=>{
     setAge(age +1)
+  }
+  const decreaseAge =()=>{
+    setAge(age -1)
   }
 
   return<div className='App'>
     <h1>{age}</h1>
     <button onClick={increaseAge}>Increase age</button>
+    <button onClick={decreaseAge}> Decearese age</button>
   </div>
 
 }
