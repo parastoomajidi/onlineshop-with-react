3c8cbc6d086a9e9c700cbe71cba80915bf3098ee
Hsis8hJ39@&@£×92992ajue92024 <parasto.majidi0@gmail.com>
2025-08-03 08:46:36 +0330

    Daily Practise-Task 4

--- src/App.js
+++ src/App.js
@@ -26,22 +26,41 @@ function App() {
   //   <input type='text' onChange={handelInputChange}></input>
   //   <h1>{inputvalue}</h1>
   // </div>
+////////////////////////////////////////////////////////////////
+//   const [showText, setShowText]= useState(true);
 
-  const [showText, setShowText]= useState(true);
+//   const toggelText =()=>{
+//     // update stat
+//     setShowText(!showText);
 
-  const toggelText =()=>{
-    // update stat
-    setShowText(!showText);
+//   return<div className='App'>
+//     <button onClick={toggelText} > show/heide</button>
+//     {showText && <h1>Parastoo Style codeing</h1>}
+//   </div>
+///////////////////////////////////////////////////////////
+///task 4
+const[count, setCount] = useState(0);
 
-
-  return<div className='App'>
-    <button onClick={toggelText} > show/heide</button>
-    {showText && <h1>Parastoo Style codeing</h1>}
-
-
-  </div>
-
+const increaseCount =()=>{
+  setCount(count+1)
 }
+const decreseCount =()=>{
+  setCount(count-1)
+}
+
+const resetCount =()=>{
+  setCount(0);
+}
+return(
+  <div className='App'>
+    <button onClick={increaseCount} >increase</button>
+    <button onClick={decreseCount}>decrease</button>
+    <button  onClick={resetCount}>set to 0</button>
+    <h1>{count}</h1>
+  </div>
+);
+}
+
 
 export default App;
 
