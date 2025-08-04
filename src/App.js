57609a08375481ccc8a46067def2908cb2f53208
Hsis8hJ39@&@£×92992ajue92024 <parasto.majidi0@gmail.com>
2025-08-04 15:25:42 +0330

    Daily Practise-CRAD list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,54 @@ import {useState} from 'react';
 
 // task 2 map the list
 function App() {
+  // **add crad item project
+  const [coursList, setCourseList] = useState([]);
+  // we create another stat for accsess the details of what ever is in input
+  const [newCourse, setNewCourse] = useState("");
+
+  const handelChange = (event)=>{
+    setNewCourse(event.target.value)
+  }
+
+
+
+
+// add course list
+    const addCourse =() =>{
+      // we create a new paramether for the compate 2 word that the are same
+      
+
+    const newCourseList = [...coursList, newCourse]
+    setCourseList(newCourseList)
+  }
+  // we are delte the course in here
+  const deleteCourse =(courseName) =>{
+    const newCourseList = coursList.filter((course)=>{
+      return courseName !== course
+    })
+    setCourseList(newCourseList)
+
+  }
+  return(
+    <div className='App'>
+      <div className=' add-course'>
+        <input type='text' onChange={handelChange}></input>
+        <button onClick={addCourse}>Add Course</button>
+      </div>
+      <div className='list'>
+        {coursList.map((course)=>{
+          return (
+          <div>
+            <h1>{course}</h1>
+            <button onClick={()=>deleteCourse(course)}>x</button>
+          </div>
+        )})}
+      </div>
+
+    </div>
+  )
+
+}
   // const [age, setAge] = useState(0);
   // const increaseAge = ()=>{
   //   setAge(age +1)
@@ -38,28 +86,28 @@ function App() {
 //     {showText && <h1>Parastoo Style codeing</h1>}
 //   </div>
 ///////////////////////////////////////////////////////////
-///task 4
-const[count, setCount] = useState(0);
+// ///task 4
+// const[count, setCount] = useState(0);
 
-const increaseCount =()=>{
-  setCount(count+1)
-}
-const decreseCount =()=>{
-  setCount(count-1)
-}
+// const increaseCount =()=>{
+//   setCount(count+1)
+// }
+// const decreseCount =()=>{
+//   setCount(count-1)
+// }
 
-const resetCount =()=>{
-  setCount(0);
-}
-return(
-  <div className='App'>
-    <button onClick={increaseCount} >increase</button>
-    <button onClick={decreseCount}>decrease</button>
-    <button  onClick={resetCount}>set to 0</button>
-    <h1>{count}</h1>
-  </div>
-);
-}
+// const resetCount =()=>{
+//   setCount(0);
+// }
+// return(
+//   <div className='App'>
+//     <button onClick={increaseCount} >increase</button>
+//     <button onClick={decreseCount}>decrease</button>
+//     <button  onClick={resetCount}>set to 0</button>
+//     <h1>{count}</h1>
+//   </div>
+// );
+// }
 
 
 export default App;
