a2d25819852cda75443981026271dda62e07ff7b
Hsis8hJ39@&@£×92992ajue92024 <parasto.majidi0@gmail.com>
2025-08-11 09:51:25 +0330

    Daily Practise-add validation with YUP package to inputs

--- src/Pages/SubmitForm.js
+++ src/Pages/SubmitForm.js
@@ -1,8 +1,22 @@
 import { useForm } from "react-hook-form"
+import * as yup from 'yup'
 export const SubmitForm=()=>{
+    const schemea = yup.object().shape({
+        name:  yup.string().required(),
+        email: yup.string().email().required(),
+        age:   yup.number().positive().min(18).max(50).required(),
+        password : yup.string().min(4).max(15).required(),
+        confirmPassword : yup.string().oneOf([yup.ref("password")]).required()
+    })
+
+
     const {register, handleSubmit} = useForm()
+
+
+
     const onFormSubmit=(data)=>{
         console.log("the form is submited")
+        console.log(data)
     }
     return(
         <form onSubmit={handleSubmit(onFormSubmit)}> 
