80129fde477b47acd6a1f1850a1a79f4210ed9b1
Hsis8hJ39@&@£×92992ajue92024 <parasto.majidi0@gmail.com>
2025-08-12 08:37:26 +0330

    daily practise-add error handeling

--- src/Pages/SubmitForm.js
+++ src/Pages/SubmitForm.js
@@ -1,16 +1,19 @@
 import { useForm } from "react-hook-form"
 import * as yup from 'yup'
+import {yupResolver} from "@hookform/resolver/yup"
+
 export const SubmitForm=()=>{
     const schemea = yup.object().shape({
-        name:  yup.string().required(),
-        email: yup.string().email().required(),
+        name:  yup.string().required("requeired field!"),
+        email: yup.string().email("the email format is wrong").required("requeired field!"),
         age:   yup.number().positive().min(18).max(50).required(),
-        password : yup.string().min(4).max(15).required(),
-        confirmPassword : yup.string().oneOf([yup.ref("password")]).required()
+        password : yup.string().min(4).max(15).required(),   
+        confirmPassword : yup.string().oneOf([yup.ref("password")],"password not match!").required()
     })
+    // regular expresstion
 
 
-    const {register, handleSubmit} = useForm()
+    const {register, handleSubmit, formState:{errors}} = useForm({resolver: yupResolver(schemea)})
 
 
 
@@ -21,10 +24,19 @@ export const SubmitForm=()=>{
     return(
         <form onSubmit={handleSubmit(onFormSubmit)}> 
             <input type="text" placeholder="name..." {...register("name")}/>
+            {errors.name &&(<p>errors.name? .message</p>)}
+            
             <input type="text" placeholder="email"{...register("email")}/>
+            {errors.email &&(<p>errors.name? .message</p>)}
+
             <input type="number" placeholder="age"{...register("age")}/>
+            {errors.age &&(<p>errors.name? .message</p>)}
+
             <input type="password" placeholder="password"{...register("password")}/>
+            {errors.password &&(<p>errors.name? .message</p>)} 
+
             <input type="password" placeholder="confirm password"{...register("confrm password")}/>
+            {errors.password &&(<p>errors.name? .message</p>)}
             <input type="submit"/>
         </form>
     )
